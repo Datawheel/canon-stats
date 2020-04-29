f1e2fac3b733cf2f506ae1b239694ef6951d034a
cnavarreteliz <cnavarreteliz@gmail.com>
2020-04-29 11:15:42 -0400

    updates package version

--- app/api/baseRoute.js
+++ app/api/baseRoute.js
@@ -83,7 +83,7 @@ module.exports = function(app) {
   });
 
   app.get(`${BASE_URL}/version`, (req, res) => {
-    return res.json({endpoints: options, version: "0.2.7"})
+    return res.json({endpoints: options, version: "0.3.0"})
   });
 
 }
