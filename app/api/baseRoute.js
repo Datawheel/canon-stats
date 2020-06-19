c42aab7cf613d7ba6d2c0cfc6f334f392511ee81
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-06-19 16:19:09 -0400

    compiles 0.4.1

--- app/api/baseRoute.js
+++ app/api/baseRoute.js
@@ -108,7 +108,7 @@ module.exports = function(app) {
   });
 
   app.get(`${BASE_URL}/version`, (req, res) => {
-    return res.json({endpoints: options, version: "0.4.0"})
+    return res.json({endpoints: options, version: "0.4.1"})
   });
 
 }
