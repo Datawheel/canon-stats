0d28ce3fc3774f2ad86d406005fa1a5a536b12c7
Alexander Simoes <alexandersimoes@gmail.com>
2020-07-20 11:17:17 -0400

    changes spawnSync back to spawn and adds more reliable timeout

--- src/api/baseRoute.js
+++ src/api/baseRoute.js
@@ -7,7 +7,6 @@ const {
   CANON_PORT,
   CANON_STATS_API,
   CANON_STATS_BASE_URL,
-  CANON_STATS_MAX_BUFFER,
   CANON_STATS_PYTHON_ENGINE,
   CANON_STATS_TIMEOUT,
   OLAP_PROXY_SECRET
@@ -15,9 +14,10 @@ const {
 
 const api = CANON_STATS_API;
 const port = CANON_PORT || "8000";
-const spawn = require("child_process").spawnSync;
-const timeout = CANON_STATS_TIMEOUT * 1 || 0;
-const maxBuffer = CANON_STATS_MAX_BUFFER * 1 || 4096 * 4096;
+const spawn = require("child_process").spawn;
+const timeout = CANON_STATS_TIMEOUT * 1;
+let timeoutId = null;
+let timedOut = false;
 
 const BASE_URL = CANON_STATS_BASE_URL || "/api/stats";
 const ENGINE = CANON_STATS_PYTHON_ENGINE || "python3";
@@ -45,14 +45,15 @@ const serverApiToken = OLAP_PROXY_SECRET ? jwt.sign({
   auth_level: 10,
   sub: "server",
   status: "valid"
-}, OLAP_PROXY_SECRET, { expiresIn: "30m" }) : "";
+}, OLAP_PROXY_SECRET, {expiresIn: "30m"}) : "";
 
 module.exports = function (app) {
   Object.entries(options).forEach(d => {
     d[1].forEach(endpoint => {
       app.get(`${BASE_URL}/${endpoint}`, (req, res) => {
-        const { headers, query, user } = req;
-        const { apiToken, authLevel } = OLAP_PROXY_SECRET ? getApiToken(headers, user) : { authLevel: 0 };
+        const {headers, query, user} = req;
+        const {debug} = req.query;
+        const {apiToken, authLevel} = OLAP_PROXY_SECRET ? getApiToken(headers, user) : {authLevel: 0};
 
         const config = OLAP_PROXY_SECRET ? {
           "x-tesseract-jwt-token": apiToken
@@ -67,18 +68,33 @@ module.exports = function (app) {
               apiQuery = JSON.stringify(query);
 
         const pyPath = path.join(__dirname, `../${d[0]}_endpoints.py`);
-        const py = spawn(ENGINE, ["-W", "ignore", pyPath, apiQuery, api, endpoint, apiHeaders, authLevel, apiServerHeaders], { maxBuffer, timeout });
 
-        if (py.signal !== "SIGTERM") {
-          const respString = py.stdout.toString();
-          const traceback = py.stderr.toString();
+        // Spawn a child process:
+        //  Using {detached : true} as a 2nd argument
+        //  allows killing of all of child's descendants.
+        //  refs: http://azimi.me/2014/12/31/kill-child_process-node-js.html
+        //        https://github.com/nodejs/node-v0.x-archive/issues/1811
+        const py = spawn(ENGINE, 
+          ["-W", "ignore", pyPath, apiQuery, api, endpoint, apiHeaders, authLevel, apiServerHeaders], 
+          {detached: true});
+        let respString = "";
+        let traceback = "";
 
-          // return response
+        // build response string based on results of python script
+        py.stdout.on("data", data => respString += data.toString());
+        
+        // catch stderr errors
+        py.stderr.on("data", data => {
+          traceback += data.toString();
+        });
+
+        // return response
+        py.stdout.on("end", () => {
           try {
             const dataResult = JSON.parse(respString);
             return res.json(dataResult);
-          } catch (error) {
-            const { debug } = req.query;
+          } 
+          catch (error) {
             const output = {
               error: error.toString()
             };
@@ -86,13 +102,60 @@ module.exports = function (app) {
             const errorCode = traceback.includes("This cube is not public") ? 401 : 404;
             return res.status(errorCode).json(output);
           }
+        });
+
+        py.on("close", () => {
+          /* If we timed_out already, nothing to do. */
+          if (timedOut) {
+            return; // timeoutId has already been called
+          }
+      
+          // Cancel the timeout timer (defined below).
+          if (timeoutId) {
+            clearTimeout(timeoutId);
+          }
+      
+          // Log when process closes
+          if (debug && debug === "true") console.log("process closed");
+        });
+
+        // ensure timeout is set and is an actual number
+        if(!isNaN(timeout)) {
+          timeoutId = setTimeout(() => {
+            console.log("---canon-stats timeout---");
+            console.log(`endpoint: ${endpoint}`);
+            console.log(`apiQuery: ${apiQuery}`);
+            console.log(err);
+            try {
+              process.kill(-py.pid, "SIGKILL");
+            } catch (e) {
+              console.log("Cannot kill canon-stats process!");
+            }
+          }, timeout);
         }
-        return res.status(408).json({ data: [], traceback: "Timeout" });
+        
+        // log the error
+        py.on("error", err => {
+          console.log("---canon-stats error---");
+          console.log(`endpoint: ${endpoint}`);
+          console.log(`apiQuery: ${apiQuery}`);
+          console.log(err);
+          console.log("---END canon-stats error---");
+        });
+
+        // make sure to clear the timeout on process exit
+        // if no timeout is set, do not clear
+        py.on("exit", () => {
+          if (timeoutId) {
+            clearTimeout(timeoutId)
+          }
+        });
+
       });
     });
   });
 
   app.get(`${BASE_URL}/version`, (req, res) => {
-    return res.json({ endpoints: options, version: "0.5.0" });
+    return res.json({endpoints: options, version: "0.5.1"});
   });
 };
