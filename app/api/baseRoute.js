e683d10d496249a501ecf1c0d9ec7e3dae2b3f66
cnavarreteliz <cnavarreteliz@gmail.com>
2020-07-27 15:31:32 -0400

    compiles v0.5.2

--- app/api/baseRoute.js
+++ app/api/baseRoute.js
@@ -154,6 +154,6 @@ module.exports = function (app) {
   });
 
   app.get(`${BASE_URL}/version`, (req, res) => {
-    return res.json({endpoints: options, version: "0.5.1"});
+    return res.json({endpoints: options, version: "0.5.2"});
   });
 };
