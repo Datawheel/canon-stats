31c4deac8a08bf68ede5a8d05f3b75dd27b94e8c
mcperez2 <47732015+mcperez2@users.noreply.github.com>
2021-06-11 18:39:38 -0300

    fix relative relatedness

--- app/complexity/relative_relatedness.py
+++ app/complexity/relative_relatedness.py
@@ -5,9 +5,13 @@ import pandas as pd
 from .relatedness import relatedness
 
 def relative_relatedness(rcas,proximities):
+    opportunity = rcas.copy()
+    opportunity[opportunity >= 1] = np.nan
+    opportunity[opportunity < 1] = 1
     wcp = relatedness(rcas,proximities)
-    wcp_mean =  wcp.mean(axis=1)
-    wcp_std =  wcp.std(axis=1)
+    wcp_opp = opportunity*wcp
+    wcp_mean =  wcp_opp.mean(axis=1)
+    wcp_std =  wcp_opp.std(axis=1)
 
     return wcp.transform(lambda x: (x-wcp_mean)/wcp_std)
     
