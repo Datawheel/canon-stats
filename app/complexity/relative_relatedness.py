c68245df423631ea2b07d1b842fbcccf4119659d
mcperez2 <47732015+mcperez2@users.noreply.github.com>
2021-06-14 06:58:59 -0300

    fix typo in relative_relatedness

--- app/complexity/relative_relatedness.py
+++ app/complexity/relative_relatedness.py
@@ -5,11 +5,12 @@ import pandas as pd
 from .relatedness import relatedness
 
 def relative_relatedness(rcas,proximities):
-    opportunity = rcas.copy()
-    opportunity[opportunity >= 1] = np.nan
-    opportunity[opportunity < 1] = 1
+    opp = rcas.copy()
+    opp[opp >= 1] = np.nan
+    opp[opp < 1] = 1
+    
     wcp = relatedness(rcas,proximities)
-    wcp_opp = opportunity*wcp
+    wcp_opp = opp*wcp
     wcp_mean =  wcp_opp.mean(axis=1)
     wcp_std =  wcp_opp.std(axis=1)
 
