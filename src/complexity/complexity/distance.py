d4236a450d6d1ed2bf7754eb0e415df6f869c066
mcperez2 <47732015+mcperez2@users.noreply.github.com>
2021-06-14 13:48:43 -0300

    compiles @datawheel/canon-stats@0.7.2

--- src/complexity/complexity/distance.py
+++
@@ -1,4 +0,0 @@
-from .relatedness import relatedness
-
-def distance(rcas, proximities):
-    return 1 - relatedness(rcas, proximities)
--- src/complexity/relative_relatedness.py
+++ src/complexity/relative_relatedness.py
@@ -2,29 +2,17 @@
 import sys
 import numpy as np
 import pandas as pd
-
+from .relatedness import relatedness
 
 def relative_relatedness(rcas,proximities):
-    rcas = rcas.copy()
-    rcas[rcas >= 1] = 1
-    rcas[rcas < 1] = 0
+    opp = rcas.copy()
+    opp[opp >= 1] = np.nan
+    opp[opp < 1] = 1
+    
+    wcp = relatedness(rcas,proximities)
+    wcp_opp = opp*wcp
+    wcp_mean =  wcp_opp.mean(axis=1)
+    wcp_std =  wcp_opp.std(axis=1)
 
-    # Get numerator by matrix multiplication of proximities with M_im
-    density_numerator = rcas.dot(proximities)
-
-    # Get denominator by multiplying proximities by all ones vector thus
-    # getting the sum of all proximities
-    # rcas_ones = pd.DataFrame(np.ones_like(rcas))
-    rcas_ones = rcas * 0
-    rcas_ones = rcas_ones + 1
-    # print rcas_ones.shape, proximities.shape
-    density_denominator = rcas_ones.dot(proximities)
-
-    # We now have our densities matrix by dividing numerator by denomiator
-    densities = density_numerator / density_denominator
-
-    wcp = densities
-    wcp_mean =  wcp.mean(axis=1)
-    wcp_std =  wcp.std(axis=1)
     return wcp.transform(lambda x: (x-wcp_mean)/wcp_std)
     
